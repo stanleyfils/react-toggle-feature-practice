6248213b57f3f0b41e0b7e82e39fcfde7875d129
Stanley Fils <stanleyfils@gmail.com>
2020-05-27 21:57:56 -0400

    Toggle React header practice complete

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import Toggle from "./Components/ToogleRPC";
 
+// anything that you want to toggle simply needs to be wrapped within the togglecomponent . This is made possible with render props/children.
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        {/* anything that you want to toggle simply needs to be wrapped within this toggle. This is made possible with render props/children.  */}
         <Toggle>
           {({ on, toggle }) => (
             <div>
-              {on && <h1>Show Me</h1>}
+              {on && (
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              )}
               <button onClick={toggle}>Show / Hide</button>
             </div>
           )}
